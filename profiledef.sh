c741e6a0a3ff94b0a6999d4d2c08d55b73463f04
kerichdev <kerichuudev@gmail.com>
2024-08-01 12:53:55 +0300

    feat: replace time with current commit checked out

--- profiledef.sh
+++ profiledef.sh
@@ -7,11 +7,12 @@
 #   IMGNAME="_homemade"
 #fi
 IMGNAME="_internal"
+GIT_COMMIT="$(git show --format="%h" --no-patch)"
 iso_name="odysendesktop"
 iso_label="ODYSEN_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Odysen <https://odysen.space>"
 iso_application="Odysen Desktop Live/Rescue ISO"
-iso_version="starman_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d-%H%M)$IMGNAME" # bringup 1.0 codename starman
+iso_version="starman_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)-$GIT_COMMIT$IMGNAME" # bringup 1.0 codename starman
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
