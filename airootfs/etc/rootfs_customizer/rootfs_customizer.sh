da2ddd4b2138366ac292e308fa2608b02c81bb01
kerichdev <kerichuudev@gmail.com>
2024-08-20 19:08:26 +0300

    feat: remove liiveuser

--- airootfs/etc/rootfs_customizer/rootfs_customizer.sh
+++ airootfs/etc/rootfs_customizer/rootfs_customizer.sh
@@ -3,20 +3,9 @@
 
 # Add a liveOS user
 ROOTPASS="config"
-LIVEOSUSER="liveuser"
 
 # Password: root
 echo -e "${ROOTPASS}\n${ROOTPASS}" | passwd root
-
-# Add 'liveuser' and set its password
-useradd --create-home ${LIVEOSUSER}; echo -e "${ROOTPASS}\n${ROOTPASS}" | passwd ${LIVEOSUSER}
-
-# Configure 'sudoers.d' for 'liveuser'
-chmod 0440 /etc/sudoers.d/${LIVEOSUSER}
-echo "${LIVEOSUSER} ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/${LIVEOSUSER}
-
-# Add necessary groups to 'liveuser'
-usermod -aG wheel,rfkill ${LIVEOSUSER}
 
 # Enable GDM (GNOME Display Manager) service and NetworkManager
 systemctl enable gdm NetworkManager
