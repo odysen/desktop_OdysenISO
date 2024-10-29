2f17e7644cbf3bc7cd50521997a23308785a3d83
kerichdev <kerichuudev@gmail.com>
2024-10-29 22:38:27 +0300

    chore: documentation in homecustomizer

--- airootfs/etc/rootfs_customizer/home_customizer.sh
+++ airootfs/etc/rootfs_customizer/home_customizer.sh
@@ -1,5 +1,8 @@
+#!/bin/zsh
+# Specify user to execute customizer process as
 LIVEOSUSER=liveuser
 
+# Customization: Set up GTK4/libadwaita themes
 cd /home/${LIVEOSUSER}/.themes/flat-remix-gtk-odysenui-dark/
 chmod +x install.sh
 ./install.sh
